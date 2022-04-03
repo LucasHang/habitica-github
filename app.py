282e8896647ffc47e93e938316c9e3d8ce126a5c
Lucas Hang <lucas.hhang@gmail.com>
2022-04-03 09:44:32 -0300

    feat: removed print

--- app.py
+++ app.py
@@ -14,51 +14,25 @@ def uptime_event():
 
 @app.route('/tasks/<task_id>/score/<direction>', methods=['POST'])
 def score_task_event(task_id, direction):
-    print('-- flask.request.environ 2.3 --')
-    print(flask.request.environ)
-
     responses = []
 
     data = flask.request.json
 
-    print('-- data --')
-    print(data)
-
     # for github integration
     for commit in data.get('commits', []):
         valid_users = _get_valid_users()
-        print('-- commit author --')
-        print(commit['author'].get('email'))
-        print('-- valid user --')
-        print(commit['author'].get('email') in valid_users)
         if commit['author'].get('email') in valid_users or not valid_users:
-            print('-- call score_task --')
-            print(task_id)
-            print(direction)
             responses.append(score_task(task_id, direction))
 
     # for habitica integration
     for historyItem in data.get('history_items', []):
         valid_users = _get_valid_users()
-        print('-- historyItem user --')
-        print(historyItem['user'].get('email'))
-        print('-- valid user --')
-        print(historyItem['user'].get('email') in valid_users)
-        print('-- after type closed --')
-        print(historyItem['after'].get('type') == 'closed')
         isValidUser = historyItem['user'].get('email') in valid_users or not valid_users
         isStatusClosed = historyItem['after'].get('type') == 'closed'
         if isValidUser and isStatusClosed:
-            print('-- call score_task --')
-            print(task_id)
-            print(direction)
             responses.append(score_task(task_id, direction))
 
-    print('-- responses --')
-    print(responses)
-
     return flask.jsonify(responses)
-
 
 def score_task(task_id, direction):
     habitica_url = 'https://habitica.com/api/v3/tasks/{}/score/{}'.format(task_id, direction)
